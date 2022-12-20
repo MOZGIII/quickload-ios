785fd70b74da06717acc85bd1d1f49648610f8be
MOZGIII <mike-n@narod.ru>
2022-12-20 20:07:13 +0400

    Initial Commit

---
+++ QuickLoad/QuickLoadApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickLoadApp.swift
+//  QuickLoad
+//
+//  Created by MOZGIII on 20.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct QuickLoadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
