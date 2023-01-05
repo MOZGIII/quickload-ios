527c1d67bf38f28abb3c84f21087d4b8f0b69e6f
MOZGIII <mike-n@narod.ru>
2023-01-06 01:59:20 +0400

    Update the rust quickload loader

--- src/lib.rs
+++ src/lib.rs
@@ -31,14 +31,25 @@ impl Manager {
         let parsed_url = url
             .parse()
             .map_err(|err| format!("unabel to parse url {:?}: {:?}", url, err))?;
-        let loader = quickload_loader::Loader::detect_size(file_path, client, parsed_url)
+        let total_size = quickload_loader::Loader::detect_size(&client, &parsed_url)
             .await
             .map_err(|err| {
                 format!(
-                    "unable to initiate the loader at {:?} for {:?}: {:?}",
+                    "unable to detect the data size at {:?} for {:?}: {:?}",
                     file_path, url, err
                 )
             })?;
+        let chunk_size = 4 * 1024 * 1024;
+        let chunk_size = chunk_size.try_into().unwrap();
+        let loader = quickload_loader::Loader::with_size(
+            file_path, client, parsed_url, total_size, chunk_size,
+        )
+        .map_err(|err| {
+            format!(
+                "unable to initiate the loader at {:?} for {:?}: {:?}",
+                file_path, url, err
+            )
+        })?;
         loader
             .run()
             .await
