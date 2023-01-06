7db4ba4328d7552cbc793a268d0f1f6c5c97de4d
MOZGIII <mike-n@narod.ru>
2023-01-06 08:23:53 +0400

    Update quickload core

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::sync::Arc;
+
 #[swift_bridge::bridge]
 mod ffi {
     extern "Rust" {
@@ -31,7 +33,7 @@ impl Manager {
         let parsed_url = url
             .parse()
             .map_err(|err| format!("unabel to parse url {:?}: {:?}", url, err))?;
-        let total_size = quickload_loader::Loader::detect_size(&client, &parsed_url)
+        let total_size = quickload_loader::detect_size(&client, &parsed_url)
             .await
             .map_err(|err| {
                 format!(
@@ -39,17 +41,23 @@ impl Manager {
                     file_path, url, err
                 )
             })?;
+        let writer = quickload_loader::init_file(file_path, total_size)
+            .map_err(|err| format!("unable to initiate the file at {:?}: {:?}", file_path, err))?;
+
         let chunk_size = 4 * 1024 * 1024;
         let chunk_size = chunk_size.try_into().unwrap();
-        let loader = quickload_loader::Loader::with_size(
-            file_path, client, parsed_url, total_size, chunk_size,
-        )
-        .map_err(|err| {
-            format!(
-                "unable to initiate the loader at {:?} for {:?}: {:?}",
-                file_path, url, err
-            )
-        })?;
+        let chunker = quickload_chunker::Chunker::<quickload_loader::ChunkerConfig> {
+            total_size,
+            chunk_size,
+        };
+
+        let loader = quickload_loader::Loader {
+            client: Arc::new(client),
+            uri: parsed_url,
+            writer,
+            chunker,
+        };
+
         loader
             .run()
             .await
